4d63954ba003bf9130da2ae76595a9e21bb2ee62
theerapunz <78726161+theerapunz@users.noreply.github.com>
2021-04-05 16:41:00 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,13 +9,14 @@ Vue.use(VueTextareaAutosize)
 Vue.config.productionTip = false
 
 firebase.initializeApp({
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+   apiKey: "AIzaSyBUItvqQXV8Cjuej-cjkZZzlf6FsL7FQu0",
+  authDomain: "bbe-booking.firebaseapp.com",
+  databaseURL: "https://bbe-booking-default-rtdb.firebaseio.com",
+  projectId: "bbe-booking",
+  storageBucket: "bbe-booking.appspot.com",
+  messagingSenderId: "550539633560",
+  appId: "1:550539633560:web:f6403552847444ab6741ed",
+  measurementId: "G-YKVLDCRQM9"
 });
 
 export const db = firebase.firestore();
